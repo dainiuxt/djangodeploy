fb7c716352b02e177300384284bd1676e7cdbade
dainiuxt <dainiuxt@gmail.com>
2024-08-04 19:05:18 +0300

    add whitenoise config

--- deployproj/settings.py
+++ deployproj/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,7 +114,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+WHITENOISE_USE_FINDERS = True
+
+if DEBUG:
+  WHITENOISE_AUTOREFRESH = True
+  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
